9940a8b23e6104ed4f3143dd3053cc5f2f41d4d4
Amber DeNeal <dominga11@users.noreply.github.com>
2014-08-09 20:07:55 -0400

    Create complete.R

---
+++ complete.R
@@ -0,0 +1,35 @@
+complete <- function(directory, id = 1:332) {
+  directory=c(getwd())
+  ResultData<-data.frame()
+  for(file in rep(id)){
+    RawData<-read.table(paste(directory,"/",sprintf("%03d", file),".csv",sep=""),sep=",",header=TRUE)
+    ResultData<-rbind(ResultData,cbind(file,nrow(RawData[complete.cases(RawData),])))
+  }
+  colnames(ResultData)<-c("id","nobs")
+  ResultData
+}
+
+
+# Output
+# source("complete.R")
+# complete("specdata", 1)
+#   id nobs
+#  1  1  117
+# complete("specdata", c(2, 4, 8, 10, 12))
+#   id nobs
+# 1  2 1041
+# 2  4  474
+# 3  8  192
+# 4 10  148
+# 5 12   96
+# complete("specdata", 30:25)
+#   id nobs
+# 1 30  932
+# 2 29  711
+# 3 28  475
+# 4 27  338
+# 5 26  586
+# 6 25  463
+# complete("specdata", 3)
+#    id nobs
+#  1  3  243
